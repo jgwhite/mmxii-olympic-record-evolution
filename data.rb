28992b5763178bccc2688b9796c99dd7b39afc22
Jamie White <jamie@jgwhite.co.uk>
2012-07-29 10:12:52 +0100

    Normalize country codes (closes #1)

--- data.rb
+++ data.rb
@@ -8,13 +8,13 @@ class DataSet
   include Mongoid::Document
 
   field :year, :type => Integer
-  field :country
+  field :country_code
+  field :country_name
   field :gdp_per_capita, :type => Float
   field :gold_medals, :type => Integer, :default => 0
   field :silver_medals, :type => Integer, :default => 0
   field :bronze_medals, :type => Integer, :default => 0
   field :total_medals, :type => Integer, :default => 0
-
 end
 
 class HackData
@@ -25,13 +25,14 @@ class HackData
     olympic_results.each do |entry|
       next if entry['year'] < 1960
 
-      puts "Adding #{entry['country']} #{entry['year']}"
+      puts "Adding #{entry['country_code']} #{entry['year']}"
 
       data_set = DataSet.find_or_initialize_by(
         year: entry['year'],
-        country: entry['country']
+        country_code: entry['country_code']
       )
       data_set.update_attributes(
+        country_name: entry['country_name'],
         gold_medals: entry['gold'],
         silver_medals: entry['silver'],
         bronze_medals: entry['bronze'],
@@ -41,17 +42,20 @@ class HackData
 
     CSV.open('sources/gdp-per-capita.csv', 'r') do |csv|
       csv.each do |row|
-        country = row[2]
-        next if country == 'Country Code'
+        country_code = row[2]
+        next if country_code == 'Country Code'
 
         year  = 1960
 
         while year <= 2008
-          puts "Adding #{country} #{year}"
+          puts "Adding #{country_code} #{year}"
           offset = (year - 1960) + 3
           if offset >= 0
-            entry = DataSet.find_or_initialize_by country: country, year: year
-            entry.update_attributes :gdp_per_capita => row[offset]
+            entry = DataSet.find_or_initialize_by country_code: country_code, year: year
+            entry.update_attributes(
+              :gdp_per_capita => row[offset],
+              :country_name => row[1]
+            )
           end
           year += 4
         end
--- scrape.rb
+++ scrape.rb
@@ -3,6 +3,18 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
+country_codes_page = Nokogiri::HTML open 'http://wits.worldbank.org/WITS/wits/WITSHELP/Content/Codes/Country_Codes.htm'
+country_codes = {}
+country_codes_page.css('.WT1 tr').each do |row|
+  paras = row.css 'p'
+  name = paras[0].content.strip
+  next if name == 'Country Name'
+  code = paras[1].content.strip
+  country_codes[name] = code
+end
+
+puts country_codes
+
 BASE = 'http://www.databaseolympics.com'
 
 index = Nokogiri::HTML open BASE
@@ -14,9 +26,12 @@ index.css('.pt10')[1].css('a').each do |link|
   page = Nokogiri::HTML open BASE + url
   page.css('.cl2').each do |row|
     cells = row.css 'td'
+    country_name = cells[0].at_css('a').content
+    country_code = country_codes[country_name] || cells[0].at_css('a')['href'].match(/cty=([A-Z]+)/)[1]
     data = {
       year: page.at_css('.nme b').content[/\d+/].to_i,
-      country: cells[0].at_css('a')['href'].match(/cty=([A-Z]+)/)[1],
+      country_code: country_code,
+      country_name: country_name,
       gold: cells[1].content,
       silver: cells[2].content,
       bronze: cells[3].content,
